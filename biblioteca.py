6d1588ba763f2763587b89167189d8c64493f3bb
LUTS <143232895+tropadoeurico@users.noreply.github.com>
2025-07-21 13:39:44 +0100

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,51 +1,51 @@
-lista1 = [] # Adicionar à lista dos id.
-lista2 = [] # Adicionar à lista dos titulos.
-lista3 = [] # Adicionar à lista dos autores.
-lista4 = [] # Adicionar à lista dos ids dos livros emprestados.
-lista5 = [] # Adicionar à lista dos titulos dos livros emprestados.
-lista6 = [] # Adicionar à lista dos autores dos livros emprestados.
+list1 = [] # Add to list of ids.
+list2 = [] # Add to list of titles.
+list3 = [] # Add to authors list.
+list4 = [] # Add to list of borrowed book IDs.
+list5 = [] # Add to list of borrowed book titles.
+list6 = [] # Add to the list of authors of borrowed books.
 
-opcao = ('1,2,3,4,5,6')
+option = ('1,2,3,4,5,6')
 
-biblioteca = [lista1, lista2, lista3]
-emprestados = [lista4, lista5, lista6]
+library = [list1, list2, list3]
+borrowed = [list4, list5, list6]
 
 while True:
     print("""
     _________________________________
-    |          BIBLIOTECA           |
+    |            LIBRARY            |
     |-------------------------------|
-    |   [1] - Adicionar um Livro.   |
-    |   [2] - Remover um Livro.     |
-    |   [3] - Emprestar um Livro.   |
-    |   [4] - Devolver um Livro.    |
-    |   [5] - Ver a Biblioteca.     |
-    |   [6] - Sair                  |
+    |   [1] - Add a book.           |
+    |   [2] - Remove a book.        |
+    |   [3] - Borrow a book.        |
+    |   [4] - Return a book.        |
+    |   [5] - View the library.     |
+    |   [6] - Leave.                |
     |_______________________________|
     """)
 
-    resposta = input('Digite uma opção: ')
+    resposta = input('Enter an option: ')
 
     if resposta in opcao:
-        if resposta == '1': # Adicionar um Livro.
-            adicionar_livro(lista1, lista2, lista3)
+        if resposta == '1': # Add a Book.
+            add_book(list1, list2, list3)
 
-        if resposta == '2': # Remover um Livro.
-            remover_livro(lista1, lista2, lista3)
+        if resposta == '2': # Remove a Book.
+            remove_book(list1, list2, list3)
 
-        if resposta == '3': # Emprestar um Livro.
-            emprestar_livro(lista1, lista2, lista3, lista4, lista5, lista6)
+        if resposta == '3': # Borrow a Book.
+            borrow_book(list1, list2, list3, list4, list5, list6)
 
-        if resposta == '4': # Devolver um Livro.
-            devolver_livro(lista1, lista2, lista3, lista4, lista5, lista6)
+        if resposta == '4': # Return a Book.
+            return_book(list1, list2, list3, list4, list5, list6)
 
-        if resposta == '5': # Ver a Biblioteca.
-            ver_livro(biblioteca)
+        if resposta == '5': # View the Library.
+            view_book(library)
 
-        if resposta == '6': # Sair.
-            print('O menu foi fechado!')
+        if resposta == '6': # Leave.
+            print('The menu has been closed!')
 
             break
 
-    else: # Repetir o programa.
-            print('Essa opção não é válida.')
+    else: # Repeat the program.
+            print('This option is not valid.')
